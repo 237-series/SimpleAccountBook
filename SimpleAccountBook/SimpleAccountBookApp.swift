507bd378dd80b8426b9571b74d345cf288985665
sglee237 <sglee237@gmail.com>
2023-01-18 17:46:23 +0900

    Initial Commit

---
+++ SimpleAccountBook/SimpleAccountBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAccountBookApp.swift
+//  SimpleAccountBook
+//
+//  Created by sglee237 on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAccountBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
