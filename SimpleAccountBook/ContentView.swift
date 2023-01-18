392ded41ab1a82ba633e68c6d849d2d1966b62a1
sglee237 <sglee237@gmail.com>
2023-01-18 18:19:27 +0900

    0. 기본 레이아웃

--- SimpleAccountBook/ContentView.swift
+++ SimpleAccountBook/ContentView.swift
@@ -7,13 +7,60 @@
 
 import SwiftUI
 
+struct MainScrollView: View {
+    var body: some View {
+        ScrollView() {
+            VStack {
+                ForEach(1..<100) {
+                    Text("Item \($0)")
+                        .font(.title)
+                }
+            }
+            .frame(maxWidth: .infinity)
+        }
+    }
+}
+
+struct AccountRow: View {
+    var body: some View {
+        HStack {
+            // 로고 자리 (이모지로 대체)
+            EmptyView()
+            
+            VStack {
+                //타이틀, 금액
+            }
+            
+            // Button 자리
+            EmptyView()
+        }
+    }
+}
+
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack { // Top Area
+                Button {
+                    
+                } label: {
+                    Text("Top Button")
+                }
+            }.padding()
+            
+            Spacer() // Contents Area
+            MainScrollView()
+            Spacer()
+            
+            
+            HStack { // Bottom Area
+                Button {
+                    
+                } label: {
+                    Text("Bottom")
+                }
+            }
         }
         .padding()
     }
