7729d9abcdc8a26e2bb92704cf7ba46d74e0ce6a
sglee237 <sglee237@gmail.com>
2023-01-23 18:51:47 +0900

    Making AccountDataManager File Seperate

---
+++ SimpleAccountBook/AccountDatamanager.swift
@@ -0,0 +1,17 @@
+//
+//  AccountDatamanager.swift
+//  SimpleAccountBook
+//
+//  Created by sglee237 on 2023/01/22.
+//
+
+import Foundation
+
+class AccountDataManager {
+    static let LIST_KEY_DEFAULT = "default"
+    
+    static let shared = AccountDataManager()
+    
+    
+
+}
--- SimpleAccountBook/ContentView.swift
+++ SimpleAccountBook/ContentView.swift
@@ -7,50 +7,6 @@
 
 import SwiftUI
 
-enum AccountCategory:String, CaseIterable {
-    //case 💸, 💰, ☕️, 🥘
-    case none
-    case saving //
-    case drink
-    case food
-    case transport
-    
-    var DisplayImoji: String {
-        //"💸", "💰", "☕️", "🥘"
-        switch self {
-        case .saving: return "💰"
-        case .drink: return "☕️"
-        case .food: return "🥘"
-        case .transport: return "🚘"
-        default: return "💸"
-        }
-    }
-    
-    var Display: String {
-        switch self {
-        case .saving: return "돈아끼기좋은날"
-        case .drink: return "커피/음료수좋은날"
-        case .food: return "먹기좋은날"
-        case .transport: return "타고이동하기"
-        default: return "돈쓰기좋은날"
-        }
-    }
-}
-
-class AccountData {
-    
-    var category:AccountCategory = .none
-    var title:String = ""
-    var account:String = "0"
-    var date:Date = Date()
-    
-    init(category: AccountCategory, title: String, account: String) {
-        self.category = category
-        self.title = title
-        self.account = account
-    }
-    init() {}
-}
 
 let DummyData: [AccountData] = [
     AccountData(category: .food, title: "저녁먹었음", account: "30,000"),
